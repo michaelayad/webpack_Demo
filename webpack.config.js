ec456b3a5474235641d5fdd43c6f63e3ebeae5fc
michael ayad <micheltwfik@gmail.com>
2022-11-22 22:36:46 +0200

    maikoool

--- webpack.config.js
+++ webpack.config.js
@@ -1,84 +1,37 @@
-const pathModule = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const pathModule = require("path");
+const HTMLWebPack = require("html-webpack-plugin");
+const miniCssExtractPlugin = require("mini-css-extract-plugin");
+const cssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
   output: {
-    filename: "script.bundle.js",
-    path: pathModule.resolve(__dirname, "dist"),
-    assetModuleFilename: 'images/[name][ext]'
+    filename: "boundle.js",
+    path: pathModule.resolve(__dirname, "build"),
+    assetModuleFilename: "assets/[name][ext]",
   },
+  mode: "development",
   module: {
     rules: [
-      //css files
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [miniCssExtractPlugin.loader, "css-loader"],
       },
-      //images
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
       {
         test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          // "style-loader",
-          //or
-          // Creates css separate file 
-          MiniCssExtractPlugin.loader,
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
-  plugins: [new HtmlWebpackPlugin(), new MiniCssExtractPlugin("style.min.css"),
- 
-  ],
   optimization: {
-    minimizer: [
-      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
-      "...",
-      new CssMinimizerPlugin(),
-      new ImageMinimizerPlugin({
-        minimizer: {
-          implementation: ImageMinimizerPlugin.imageminMinify,
-          options: {
-            // Lossless optimization with custom option
-            // Feel free to experiment with options for better result for you
-            plugins: [
-              ["gifsicle", { interlaced: true }],
-              ["mozjpeg", { quality: 60 }],
-              ["optipng", { optimizationLevel: 5 }],
-              // Svgo configuration here https://github.com/svg/svgo#configuration
-              [
-                "svgo",
-                {
-                  name: 'preset-default',
-                  params: {
-                    overrides: {
-                      // customize plugin options
-                      convertShapeToPath: {
-                        convertArcs: true
-                      },
-                      // disable plugins
-                      convertPathData: false
-                    }
-                  }
-                }
-              ],
-            ],
-          },
-        },
-      }),
-    ],
-  }
-
-}
+    minimizer: [new cssMinimizerPlugin()],
+  },
+  plugins: [
+    new HTMLWebPack({ template: "./src/index.html" }),
+    new miniCssExtractPlugin({ filename: "style.min.css" }),
+    new cssMinimizerPlugin(),
+  ],
+};
